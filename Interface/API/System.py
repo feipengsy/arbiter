b0792425d96cec8011bfdb75d5136dd7961ad8e6
LiTeng <liteng_shiyan@163.com>
2013-06-04 15:14:11 +0800

    normalize the code

--- Interface/API/System.py
+++ Interface/API/System.py
@@ -1,3 +1,14 @@
+"""
+  ARBITER API class  
+  All arbiter functionality is exposed through the arbiter API
+
+  The system API provides the following functionality:
+  - Get the workflow list that were generated
+  - Execute workflow that were generated or resubmit workflow or sub-jobs in it.
+  - get the status of workflow
+"""
+
+
 from arbiter.Core.Utilities.ReturnValues import *
 from arbiter.Core.Utilities.Utilities import *
 from arbiter.Core.Utilities.Constants import *
@@ -13,26 +24,43 @@ class system:
     self.tempDirectory = tempDirectory
     self.dbTool = dbTool()
     self.optionfiles = []
+    #get the user name of batch farm $USER
     self.user = getUserName()
 
   def listJob( self, user ):
+   """
+      Get the workflow list
+      returns: S_OK,S_ERROR
+   """
     jobList = self.dbTool.getJobList( user )
-    return jobList
+    return S_OK( jobList )
 
   def generate( self, jobNum ):
+   """
+      Generate option files of all steps
+   """
     xmlPath = self.tempDirectory + str( jobNum ) + '.xml'
     job = Job( '', xmlPath )
     job.generate()
+    return S_OK()
 
   def execute( self, jobNum ):
+   """
+     Execute the first step of the workflow locally
+   """
     xmlPath = self.tempDirectory + str( jobNum ) + '.xml'
     job = Job( '', xmlPath )
     job.execute()
+    return S_OK()
 
   def submit( self, jobNum ):
+   """
+     Submit the first step of the workflow to the queue
+   """
     xmlPath = self.tempDirectory + str( jobNum ) + '.xml'
     job = Job( '', xmlPath )
     job.submit()
+    return S_OK()
 
   def resubmit( self, infoDict ):
     jobNum = infoDict['jobNum']
