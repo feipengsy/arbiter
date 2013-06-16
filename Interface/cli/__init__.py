b238151ef73a0a83b21501a61422e726e6e86b1e
= <=>
2013-06-16 15:37:02 +0800

    added sub commands

--- Interface/cli/__init__.py
+++ Interface/cli/__init__.py
@@ -1,3 +1,6 @@
 from arbiter.Interface.cli.arb_checkstatus import *
 from arbiter.Interface.cli.arb_listworkflow import *
 from arbiter.Interface.cli.arb_resubmit import *
+from arbiter.Interface.cli.arb_delete import *
+from arbiter.Interface.cli.arb_generate import *
+from arbiter.Interface.cli.arb_submit import *
